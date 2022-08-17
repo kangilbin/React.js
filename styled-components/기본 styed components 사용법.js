f70d866a17bc5eb9253ac65d9f261d0affa045f4
kangilbin <kib3388@naver.com>
2022-08-18 00:01:17 +0900

    styled-components

---
+++ styled-components/기본 styed components 사용법.js
@@ -0,0 +1,13 @@
+import TodoList from "./TodoList";
+import CoinTracker from "./CoinTracker";
+import Movie from "./Movie";
+
+function App() {
+  return (
+    <div>
+      <Movie />
+    </div>
+  );
+}
+
+export default App;
