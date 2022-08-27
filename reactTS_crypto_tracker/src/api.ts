7f35046e8bc5e2cbe11a4f7e4f42d55f119ba7dd
kangilbin <kib3388@naver.com>
2022-08-27 23:31:43 +0900

    종가 api

--- reactTS_crypto_tracker/src/api.ts
+++ reactTS_crypto_tracker/src/api.ts
@@ -18,8 +18,8 @@ export function fetchCoinTickers(coinId: string) {
 
 export function fetchCoinHistory(coinId: string) {
   const endDate = Math.floor(Date.now() / 1000);
-  const starDate = endDate;
-  return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical`).then(
-    (response) => response.json()
-  );
+  const starDate = endDate - 60 * 60 * 24 * 7 * 2;
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}&start=${starDate}&end=${endDate}`
+  ).then((response) => response.json());
 }
--- reactTS_crypto_tracker/src/routes/Chart.tsx
+++ reactTS_crypto_tracker/src/routes/Chart.tsx
@@ -1,12 +1,16 @@
 import { useOutletContext } from "react-router";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart() {
-  const data = useOutletContext<ChartProps>();
-  console.log(data);
+  const coinData = useOutletContext<ChartProps>();
+  const { isLoading, data } = useQuery(["ohlcv", coinData.coinId], () =>
+    fetchCoinHistory(coinData.coinId)
+  );
   return <h1>Chart</h1>;
 }
 
