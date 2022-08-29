f0f1b4b30e7cd304ab8d6873871837197f5294a6
kangilbin <kib3388@naver.com>
2022-08-29 23:15:57 +0900

    차트 추가

--- reactTS_crypto_tracker/src/routes/Chart.tsx
+++ reactTS_crypto_tracker/src/routes/Chart.tsx
@@ -1,17 +1,59 @@
-import { useOutletContext } from "react-router";
 import { useQuery } from "react-query";
+import { useOutletContext } from "react-router";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  high: number;
+  open: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 function Chart() {
-  const coinData = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery(["ohlcv", coinData.coinId], () =>
-    fetchCoinHistory(coinData.coinId)
+  const { coinId } = useOutletContext<ChartProps>();
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            { name: "Price", data: data?.map((price) => price.close) ?? [] },
+            { name: "data2", data: [15, 18, 15, 78, 56, 16] },
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            stroke: { curve: "smooth", width: 4 },
+            grid: { show: false },
+            yaxis: { show: false },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
