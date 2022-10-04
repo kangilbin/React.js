9614ec23aecb444327af1510a6344826f0f43a4d
kangilbin <kib3388@naver.com>
2022-10-04 23:14:02 +0900

    NextJS

---
+++ nextjs-study/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+}
