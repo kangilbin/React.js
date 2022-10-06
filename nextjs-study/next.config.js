135685f4c44b8e0285ad5c68f8a7a99979ef83fc
kangilbin <kib3388@naver.com>
2022-10-06 21:29:01 +0900

    redirect 와 rewrites

--- nextjs-study/next.config.js
+++ nextjs-study/next.config.js
@@ -1,7 +1,28 @@
+const { redirect } = require("next/dist/server/api-utils");
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        // permanent(영구적)인지 아니니지에 따라 브라우저나, 검색엔진이 이 정보를 기억하는 여부를 결정된다.
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
