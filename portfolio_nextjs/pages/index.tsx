13c3509c89dd54fbc57656f59f6192256b17d10d
kangilbin <kib3388@naver.com>
2022-10-12 21:10:55 +0900

    슬라이드 틀

--- portfolio_nextjs/pages/index.tsx
+++ portfolio_nextjs/pages/index.tsx
@@ -1,5 +1,7 @@
 import Seo from "./components/Seo";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Container = styled.div`
   display: grid;
@@ -17,13 +19,52 @@ const BigBox = styled.div`
   gap: 20px;
   grid-template-columns: repeat(4, 1fr);
 `;
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 20vw;
   height: 40vh;
   background-color: black;
   border-radius: 50px;
 `;
+
+interface IBack {
+  isBack: boolean;
+}
+
+const variantBox = {
+  entry: ({ isBack }: IBack) => ({
+    x: isBack ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: ({ isBack }: IBack) => ({
+    x: isBack ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 0.5,
+    },
+  }),
+};
+
 const Home = () => {
+  const [visible, setVisible] = useState(1);
+  const [isBack, setIsBack] = useState(false);
+  const nextPlease = () => {
+    setIsBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setIsBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <>
       <Seo title="Home" />
@@ -31,11 +72,19 @@ const Home = () => {
         <Grid>자기소개</Grid>
         <Grid>
           <BigBox>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
+            <AnimatePresence custom={{ isBack }}>
+              <Box
+                custom={{ isBack }}
+                variants={variantBox}
+                initial="entry"
+                animate="center"
+                exit="exit"
+                key={visible}
+              />
+            </AnimatePresence>
           </BigBox>
+          <button onClick={nextPlease}>next</button>
+          <button onClick={prevPlease}>prev</button>
         </Grid>
       </Container>
     </>
